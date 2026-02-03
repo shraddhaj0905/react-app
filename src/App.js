cad47047280bf430a90a47c7f01071d1b7990032
shraddhaj0905 <shraddhajadhav0905@gmail.com>
2026-02-03 11:28:24 +0530

    Update entire project with frontend and amplify backend

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
+import { useEffect, useState } from "react";
+import type { Schema } from "../amplify/data/resource";
+import { generateClient } from "aws-amplify/data";
 
-import Header from "./components/Header"
-import Blogs from "./components/Blogs"
-import Pagination from "./components/Pagination"
-import { useContext, useEffect } from "react"
-import { AppContext } from "./contex/AppContext"
-export default function App() {
-  const {fetchBlogPosts}=useContext(AppContext);
+const client = generateClient<Schema>();
 
-  useEffect(()=>{
-    fetchBlogPosts();
-  },[]);
+function App() {
+  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+
+  useEffect(() => {
+    client.models.Todo.observeQuery().subscribe({
+      next: (data) => setTodos([...data.items]),
+    });
+  }, []);
+
+  function createTodo() {
+    client.models.Todo.create({ content: window.prompt("Todo content") });
+  }
+
   return (
-    <div>
-      <Header/>
-      <Blogs/>
-      <Pagination/>
-    </div>
-  )
+    <main>
+      <h1>My todos</h1>
+      <button onClick={createTodo}>+ new</button>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.content}</li>
+        ))}
+      </ul>
+      <div>
+        🥳 App successfully hosted. Try creating a new todo.
+        <br />
+        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
+          Review next step of this tutorial.
+        </a>
+      </div>
+    </main>
+  );
 }
+
+export default App;
