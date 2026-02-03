af1af9fa5ae08263e776dfc0179af66233a0f081
shraddhaj0905 <shraddhajadhav0905@gmail.com>
2026-02-03 12:01:11 +0530

    Update App import to App.tsx

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.tsx";
 import { AppContextProvider } from "./contex/AppContext"; // Fixed import
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
